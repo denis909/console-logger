b377180b83907c2792bbe9aac1e99e77b12d197f
denis909 <denis909@mail.ru>
2020-01-09 14:13:52 +0300

    n/a

--- ConsoleLogger.php
+++ ConsoleLogger.php
@@ -1,9 +1,43 @@
 <?php
-
+/**
+ * @author denis909 <dev@denis909.spb.ru>
+ * @license MIT
+ * @link http://denis909.spb.ru
+ */
 namespace Denis909\ConsoleLogger;
 
 use Psr\Log\AbstractLogger;
+use Psr\Log\LogLevel;
 
 class ConsoleLogger extends AbstractLogger
 {
+
+    public $levels = [
+        LogLevel::EMERGENCY,
+        LogLevel::ALERT,
+        LogLevel::CRITICAL,
+        LogLevel::ERROR,
+        LogLevel::WARNING,
+        LogLevel::NOTICE,
+        LogLevel::INFO,
+        LogLevel::DEBUG
+    ];
+
+    public function log($level, $message, array $context = [])
+    {
+        if (array_search($level, $this->levels) === false)
+        {
+            return;
+        }
+
+        if ($message === null)
+        {
+            print_r($context);
+
+            return;
+        }
+
+        echo strtr($message, $context) . PHP_EOL;
+    }
+
 }
